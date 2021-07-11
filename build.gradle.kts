abd81232279408c43b856835d80b8308f81e3c0b
Project_TL <me@projecttl.net>
2021-07-12 08:41:44 +0900

    style: clean DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.20"
     `maven-publish`
-
-    //`java-library`
-    //signing
 }
 
 group = properties["pluginGroup"]!!
@@ -39,67 +36,11 @@ tasks {
     }
 }
 
-/*
-tasks.register<Jar>("javadocJar") {
-    archiveClassifier.set("javadoc")
-    from(tasks.javadoc.get().destinationDir)
-}
-*/
-
 publishing {
     publications {
         create<MavenPublication>(rootProject.name) {
             from(components["java"])
             artifact(tasks["sourceJar"])
-            /*
-            artifact(tasks["javadocJar"])
-
-            val mavenUploadUser: String by project
-            val mavenUploadPwd: String by project
-
-            repositories {
-                maven {
-                    name = "MavenCentral"
-                    val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-                    val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-
-                    credentials {
-                        username = mavenUploadUser
-                        password = mavenUploadPwd
-                    }
-                }
-            }
-
-            pom {
-                name.set(rootProject.name)
-                description.set("")
-                url.set("https://github.com/ProjectTL12345/InventoryGUI/")
-
-                developers {
-                    developer {
-                        name.set("ProjectTL12345")
-                        email.set("me@projecttl.net")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:https://github.com/ProjectTL12345/InventoryGUI.git")
-                    developerConnection.set("scm:git:https://github.com/ProjectTL12345/InventoryGUI.git")
-                    url.set("https://github.com/ProjectTL12345/")
-                }
-
-            }
-            */
         }
     }
 }
-
-/*
-signing {
-    val pgpSigningKey: String? by project
-    val pgpSigningPwd: String? by project
-    useInMemoryPgpKeys(pgpSigningKey, pgpSigningPwd)
-    sign(publishing.publications["mavenJava"])
-}
- */
