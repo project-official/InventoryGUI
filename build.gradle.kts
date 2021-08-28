52cbd96ab85585b8e03846e6fd0c6b901a24b797
Project_TL <me@projecttl.net>
2021-08-28 17:57:47 +0900

    feat: update kotlin version [ 1.2.21 -> 1.5.30 ]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
     id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
 }
 
 group = "net.projecttl"
-version = "4.1.2"
+version = "4.1.3"
 
 allprojects {
     repositories {
