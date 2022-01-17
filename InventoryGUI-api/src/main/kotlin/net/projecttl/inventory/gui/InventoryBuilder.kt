cb09f9148147731faf6bc9add4e1d7b53d6ca249
Dev_Project <me@projecttl.net>
2022-01-18 01:27:39 +0900

    fix: fix not import `org.bukkit.event.inventory.InventoryCloseEvent`

--- InventoryGUI-api/src/main/kotlin/net/projecttl/inventory/gui/InventoryBuilder.kt
+++ InventoryGUI-api/src/main/kotlin/net/projecttl/inventory/gui/InventoryBuilder.kt
@@ -5,6 +5,7 @@ import net.projecttl.inventory.util.InventoryType
 import net.projecttl.inventory.util.Slot
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import java.util.*
